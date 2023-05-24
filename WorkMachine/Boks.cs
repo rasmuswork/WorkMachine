96eabe2b8d0ece3785f65539280df71ab490315a
Rasmus Peter Hjorth - Skole <rahj1@elev.tec.dk>
2023-05-24 15:29:28 +0200

    Add project files.

---
+++ WorkMachine/Boks.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkMachine
+{
+    enum BoksType
+    {
+           LilleBoks,
+           MellemBoks,
+           StorBoks,
+    }
+    public class Boks //Makes a recipe for what a box has of properties.
+    {
+        public double Height { get; set; }
+        public double Width { get; set; }
+        public double Length { get; set; }
+
+        public BoksType Type { get; set; }
+    }
+}
